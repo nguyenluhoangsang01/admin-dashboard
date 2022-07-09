c5229eec4f4276bc0a683c95829f8252512771f6
Nguyen Lu Hoang Sang <hoangsang.nguyen2001a@gmail.com>
2022-07-10 00:34:28 +0700

    Build default folders and files structures

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
+import "the-new-css-reset/css/reset.css";
 import "./App.css";
 
 function App() {
-  return <div className="App">App</div>;
+  return <h1 className="underline text-3xl">Heading</h1>;
 }
 
 export default App;
