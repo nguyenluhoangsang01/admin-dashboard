5683bcbbeadd26cdf20a91299e59734604cfd1e2
Nguyen Lu Hoang Sang <hoangsang.nguyen2001a@gmail.com>
2022-07-13 09:59:14 +0700

    Add another syncfusion packagea and create  components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,85 @@
+import { TooltipComponent } from "@syncfusion/ej2-react-popups";
+import { FiSettings } from "react-icons/fi";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "the-new-css-reset/css/reset.css";
 import "./App.css";
 
 function App() {
-  return <h1 className="underline text-3xl">Heading</h1>;
+  const activeMenu = false;
+
+  return (
+    <BrowserRouter>
+      <div className="flex relative dark:bg-main-dark-bg">
+        {/* Settings button */}
+        <div className="fixed right-4 bottom-4" style={{ zIndex: 1000 }}>
+          <TooltipComponent content={"Settings"} position="Top">
+            <button
+              type="button"
+              className="text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white"
+              style={{
+                backgroundColor: "blue",
+                borderRadius: "50%",
+              }}
+            >
+              <FiSettings />
+            </button>
+          </TooltipComponent>
+        </div>
+
+        {/* Right Active Menu */}
+        {activeMenu ? (
+          <div className="w-72 fixed dark:bg-secondary-dark-bg sidebar bg-white">
+            Sidebar
+          </div>
+        ) : (
+          <div className="w-0 dark:bg-secondary-dark-bg">Sidebar w-0</div>
+        )}
+
+        {/* Navbar */}
+        <div
+          className={`dark:bg-main-bg bg-main-bg min-h-screen w-full ${
+            activeMenu ? "md:ml-72" : "flex-2"
+          }`}
+        >
+          <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full">
+            Navbar
+          </div>
+        </div>
+
+        {/* Main Content */}
+        <div>
+          <Routes>
+            {/* Dashboard */}
+            <Route path="/" element="ECommerce" />
+            <Route path="/eCommerce" element="ECommerce" />
+
+            {/* Pages */}
+            <Route path="/orders" element="Orders" />
+            <Route path="/employees" element="Employees" />
+            <Route path="/customers" element="Customers" />
+
+            {/*Apps  */}
+            <Route path="/kanban" element="Kanban" />
+            <Route path="/editor" element="Editor" />
+            <Route path="/calendar" element="Calendar" />
+            <Route path="/color-picker" element="Color Picker" />
+
+            {/* Charts */}
+            <Route path="/line" element="Line" />
+            <Route path="/area" element="Area" />
+            <Route path="/bar" element="Bar" />
+            <Route path="/pie" element="Pie" />
+            <Route path="/financial" element="Financial" />
+            <Route path="/color-mapping" element="Color Mapping" />
+            <Route path="/pyramid" element="Pyramid" />
+            <Route path="/stacked" element="Stacked" />
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
+
+// class o dau ra, yarn add syncfusion
