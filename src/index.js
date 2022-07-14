33f5145b4614e3d9b45cb1408eb4f2dc98d0bcfa
Nguyen Lu Hoang Sang <hoangsang.nguyen2001a@gmail.com>
2022-07-14 20:31:32 +0700

    Register License for Syncfusion

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { ContextProvider } from "./contexts/ContextProvider";
 import "./index.css";
+import { registerLicense } from "@syncfusion/ej2-base";
+
+registerLicense(
+  "ORg4AjUWIQA/Gnt2VVhiQlFadVdJVXxAYVF2R2FJflRwdF9HYkwxOX1dQl9hSXlSckVnXXhec3FVTmQ="
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
