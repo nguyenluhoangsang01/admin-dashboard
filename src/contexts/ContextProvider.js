b203e83b132c86b30e14df6f6b9a09af75f28661
Nguyen Lu Hoang Sang <hoangsang.nguyen2001a@gmail.com>
2022-07-14 12:42:51 +0700

    Create Navbar component, handle click sidebar and logic for context provider

--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -2,18 +2,39 @@ const { createContext, useState, useContext } = require("react");
 
 const StateContext = createContext();
 
-// const initialState = {
-//   chat: false,
-//   cart: false,
-//   userProfile: false,
-//   notification: false,
-// };
+const initialState = {
+  cart: false,
+  chat: false,
+  notification: false,
+  userProfile: false,
+};
 
 export const ContextProvider = ({ children }) => {
   const [activeMenu, setActiveMenu] = useState(true);
+  const [isClicked, setIsClicked] = useState(initialState);
+  const [screenSize, setScreenSize] = useState(undefined);
+
+  const handleClick = (clicked) => {
+    setIsClicked((prevInitialState) => ({
+      ...prevInitialState,
+      [clicked]: !prevInitialState[clicked],
+    }));
+  };
+
+  console.log(screenSize);
 
   return (
-    <StateContext.Provider value={{ activeMenu, setActiveMenu }}>
+    <StateContext.Provider
+      value={{
+        activeMenu,
+        setActiveMenu,
+        isClicked,
+        setIsClicked,
+        handleClick,
+        screenSize,
+        setScreenSize,
+      }}
+    >
       {children}
     </StateContext.Provider>
   );
