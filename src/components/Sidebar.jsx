7f0c7b339d5b1160dc007ff00cac1da10b36f6c8
Nguyen Lu Hoang Sang <hoangsang.nguyen2001a@gmail.com>
2022-07-14 15:43:57 +0700

    Set firstLinkNameDefault

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -9,6 +9,8 @@ import { links } from "../data/dummy";
 const Sidebar = () => {
   const { activeMenu, setActiveMenu, screenSize } = useStateContext();
 
+  const firstLinkNameDefault = "ecommerce";
+
   const activeLink = `flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md m-2 bg-blue-700`;
   const normalLink = `flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2`;
 
@@ -53,7 +55,11 @@ const Sidebar = () => {
                 key={link.name}
                 to={`/${link.name}`}
                 className={({ isActive }) =>
-                  isActive ? activeLink : normalLink
+                  isActive ||
+                  (window.location.pathname === "/" &&
+                    link.name === firstLinkNameDefault)
+                    ? activeLink
+                    : normalLink
                 }
                 onClick={handleCloseSidebar}
               >
@@ -69,3 +75,5 @@ const Sidebar = () => {
 };
 
 export default Sidebar;
+
+//
