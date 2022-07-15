a0b0c9135bb1ab8983b79a7c85b38120b0fb45d7
Nguyen Lu Hoang Sang <hoangsang.nguyen2001a@gmail.com>
2022-07-15 12:32:01 +0700

    Create Kanban page

--- src/pages/Kanban.jsx
+++ src/pages/Kanban.jsx
@@ -1,7 +1,36 @@
 import React from "react";
+import {
+  KanbanComponent,
+  ColumnsDirective,
+  ColumnDirective,
+} from "@syncfusion/ej2-react-kanban";
+import { kanbanGrid, kanbanData } from "../data/dummy";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 const Kanban = () => {
-  return <div>Kanban</div>;
+  return (
+    <div>
+      <div className="m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl">
+        <Header category="app" title="kanban" />
+
+        <KanbanComponent
+          id="kanban"
+          dataSource={kanbanData}
+          cardSettings={{ contentField: "Summary", headerField: "Id" }}
+          keyField="Status"
+        >
+          <ColumnsDirective>
+            {kanbanGrid.map((item, index) => (
+              <ColumnDirective key={index} {...item} />
+            ))}
+          </ColumnsDirective>
+        </KanbanComponent>
+      </div>
+
+      <Footer />
+    </div>
+  );
 };
 
 export default Kanban;
