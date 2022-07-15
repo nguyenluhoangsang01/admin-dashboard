17ec5b3785a10e67f852a005f5d86c9bbbc767ba
Nguyen Lu Hoang Sang <hoangsang.nguyen2001a@gmail.com>
2022-07-15 11:02:41 +0700

    Create Calendar page

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -1,7 +1,38 @@
 import React from "react";
+import {
+  ScheduleComponent,
+  Day,
+  Week,
+  WorkWeek,
+  Month,
+  Agenda,
+  Resize,
+  DragAndDrop,
+  Inject,
+} from "@syncfusion/ej2-react-schedule";
+import { scheduleData } from "../data/dummy";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 const Calendar = () => {
-  return <div>Calendar</div>;
+  return (
+    <div>
+      <div className="m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl">
+        <Header category="app" title="calendar" />
+
+        <ScheduleComponent
+          height="650px"
+          eventSettings={{ dataSource: scheduleData }}
+        >
+          <Inject
+            services={[Day, Week, WorkWeek, Month, Agenda, Resize, DragAndDrop]}
+          />
+        </ScheduleComponent>
+      </div>
+
+      <Footer />
+    </div>
+  );
 };
 
 export default Calendar;
