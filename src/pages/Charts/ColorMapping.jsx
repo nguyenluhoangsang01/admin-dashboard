2b9137dfc05fc2764531d0f95362fb0df6152ed3
Nguyen Lu Hoang Sang <hoangsang.nguyen2001a@gmail.com>
2022-07-16 13:00:12 +0700

    Create ColorMapping page

--- src/pages/Charts/ColorMapping.jsx
+++ src/pages/Charts/ColorMapping.jsx
@@ -1,7 +1,76 @@
+import {
+  Category,
+  ChartComponent,
+  ColumnSeries,
+  DataLabel,
+  Inject,
+  Legend,
+  RangeColorSettingDirective,
+  RangeColorSettingsDirective,
+  SeriesCollectionDirective,
+  SeriesDirective,
+  Tooltip,
+} from "@syncfusion/ej2-react-charts";
 import React from "react";
 
+import {
+  colorMappingData,
+  ColorMappingPrimaryXAxis,
+  ColorMappingPrimaryYAxis,
+  rangeColorMapping,
+} from "../../data/dummy";
+import HeaderFooterPage from "../../layout/pages";
+
 const ColorMapping = () => {
-  return <div>ColorMapping</div>;
+  return (
+    <HeaderFooterPage
+      category="chart"
+      title="Color Mapping"
+      subtitle="USA CLIMATE - WEATHER BY MONTH"
+    >
+      <ChartComponent
+        id="color-mapping-chart"
+        primaryXAxis={ColorMappingPrimaryXAxis}
+        primaryYAxis={ColorMappingPrimaryYAxis}
+        chartArea={{ border: { width: 0 } }}
+        legendSettings={{
+          mode: "Range",
+          visible: true,
+          toggleVisibility: false,
+          background: "white",
+        }}
+        tooltip={{ enable: true }}
+      >
+        <Inject
+          services={[ColumnSeries, DataLabel, Tooltip, Category, Legend]}
+        />
+        <SeriesCollectionDirective>
+          <SeriesDirective
+            dataSource={colorMappingData[0]}
+            name="USA"
+            xName="x"
+            yName="y"
+            type="Column"
+            cornerRadius={{
+              topLeft: 10,
+              topRight: 10,
+            }}
+            marker={{
+              dataLabel: { visible: true, position: "Inner" },
+            }}
+          ></SeriesDirective>
+        </SeriesCollectionDirective>
+        <RangeColorSettingsDirective>
+          {rangeColorMapping.map((item, index) => (
+            <RangeColorSettingDirective
+              key={index}
+              {...item}
+            ></RangeColorSettingDirective>
+          ))}
+        </RangeColorSettingsDirective>
+      </ChartComponent>
+    </HeaderFooterPage>
+  );
 };
 
 export default ColorMapping;
