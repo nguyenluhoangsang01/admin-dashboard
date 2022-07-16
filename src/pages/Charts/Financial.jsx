73a809a89d5c516621cf7a8857b3ca6b9aff2d84
Nguyen Lu Hoang Sang <hoangsang.nguyen2001a@gmail.com>
2022-07-16 12:39:03 +0700

    Create Financial page

--- src/pages/Charts/Financial.jsx
+++ src/pages/Charts/Financial.jsx
@@ -1,7 +1,62 @@
+import {
+  ChartComponent,
+  Crosshair,
+  DateTime,
+  HiloSeries,
+  Inject,
+  Logarithmic,
+  SeriesCollectionDirective,
+  SeriesDirective,
+  Tooltip,
+  Zoom,
+} from "@syncfusion/ej2-react-charts";
 import React from "react";
 
+import {
+  financialChartData,
+  FinancialPrimaryXAxis,
+  FinancialPrimaryYAxis,
+} from "../../data/dummy";
+import HeaderFooterPage from "../../layout/pages";
+
 const Financial = () => {
-  return <div>Financial</div>;
+  return (
+    <HeaderFooterPage
+      category="chart"
+      title="Financial"
+      subtitle="AAPLE Historical"
+    >
+      <ChartComponent
+        id="financial-chart"
+        primaryXAxis={FinancialPrimaryXAxis}
+        primaryYAxis={FinancialPrimaryYAxis}
+        chartArea={{ border: { width: 0 } }}
+        tooltip={{ enable: true, shared: true }}
+        crosshair={{ enable: true, lineType: "Vertical", line: { width: 0 } }}
+      >
+        <Inject
+          services={[
+            HiloSeries,
+            Tooltip,
+            DateTime,
+            Logarithmic,
+            Crosshair,
+            Zoom,
+          ]}
+        />
+        <SeriesCollectionDirective>
+          <SeriesDirective
+            dataSource={financialChartData}
+            xName="x"
+            yName="low"
+            type="Hilo"
+            low="low"
+            high="high"
+          ></SeriesDirective>
+        </SeriesCollectionDirective>
+      </ChartComponent>
+    </HeaderFooterPage>
+  );
 };
 
 export default Financial;
