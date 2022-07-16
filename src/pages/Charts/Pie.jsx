6e2ca7b8138954ec49d555480bde50105527501b
Nguyen Lu Hoang Sang <hoangsang.nguyen2001a@gmail.com>
2022-07-16 12:30:19 +0700

    Create Pie page

--- src/pages/Charts/Pie.jsx
+++ src/pages/Charts/Pie.jsx
@@ -1,7 +1,69 @@
+import {
+  AccumulationChartComponent,
+  AccumulationDataLabel,
+  AccumulationLegend,
+  AccumulationSeriesCollectionDirective,
+  AccumulationSeriesDirective,
+  AccumulationTooltip,
+  Inject,
+  PieSeries,
+} from "@syncfusion/ej2-react-charts";
 import React from "react";
+import { pieChartData } from "../../data/dummy";
+
+import HeaderFooterPage from "../../layout/pages";
 
 const Pie = () => {
-  return <div>Pie</div>;
+  return (
+    <HeaderFooterPage
+      category="chart"
+      title="Pie"
+      subtitle="Project Cost Breakdown"
+    >
+      <AccumulationChartComponent
+        id="pie-chart"
+        legendSettings={{
+          background: "white",
+          visible: true,
+          position: "Top",
+        }}
+        tooltip={{ enable: true }}
+        enableSmartLabels={true}
+      >
+        <Inject
+          services={[
+            AccumulationTooltip,
+            AccumulationLegend,
+            PieSeries,
+            AccumulationDataLabel,
+          ]}
+        />
+        <AccumulationSeriesCollectionDirective>
+          <AccumulationSeriesDirective
+            dataSource={pieChartData}
+            xName="x"
+            yName="y"
+            innerRadius="40%"
+            startAngle={0}
+            endAngle={360}
+            radius="70%"
+            explode={true}
+            explodeOffset="10%"
+            explodeIndex={3}
+            dataLabel={{
+              visible: true,
+              name: "text",
+              position: "Inside",
+              font: {
+                fontWeight: "600",
+                color: "#ffffff",
+              },
+            }}
+          ></AccumulationSeriesDirective>
+        </AccumulationSeriesCollectionDirective>
+      </AccumulationChartComponent>
+    </HeaderFooterPage>
+  );
 };
 
 export default Pie;
